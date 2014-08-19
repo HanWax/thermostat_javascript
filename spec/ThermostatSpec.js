76b2553abee549b3879805a8ca9038fa97e5848c
Hannah Waxman <hannah.waxman8@gmail.com>
2014-08-19 20:53:44 +0100

    Temperature changes now take into account whether power saving mode is on

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,25 +1,95 @@
-
-
 describe("Thermostat", function() {
   var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
+  });
+
+  describe("set up", function(){
+      it("should begin at 20 degrees", function() {
+        expect(thermostat.temperature).toEqual(20);
+      });
+
+      it("should be in PSM as default", function(){
+        expect(thermostat.powerSaveMode).toBe(true);
+      })
+  });
+
+  describe('maximum temperature', function() {
+
+    it('should be 25 degrees when PSM is on', function(){
+      expect(thermostat.maximumTemperature()).toEqual(25);
+    })
+
+     it('should be 32 degrees when PSM is on', function(){
+      thermostat.powerSaveMode = false;
+      expect(thermostat.maximumTemperature()).toEqual(32);        
+    })
+  });
+
+
+describe("with PSM on", function(){
+
+    describe("increasing the temperature", function (){
+      it ("can increase the temperature if less than 25 degrees", function() {
+        thermostat.increaseTemperature()
+        expect(thermostat.temperature).toEqual(21)
+      })
+
+      it("won't increase the temperature if over 25 degrees", function(){
+        thermostat.temperature = 25
+        thermostat.increaseTemperature()
+        expect(thermostat.temperature).toEqual(25)
+      })
+    })
+
+    describe("decrease the temperature", function(){
+      it ("can decrease the temperature if current temperature is above 10 degrees", function () {
+        thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual(19)
+      });
+
+      it("should not be able to decrease below minimum temperature of 10 degrees", function() {
+        thermostat.temperature = 10
+        thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual(10)
+      });
+    })
+})
+
+describe("with PSM off", function(){
+
+  beforeEach(function() {
+    thermostat.powerSaveMode = false;
 
   });
 
-    it("should begin at 20 degrees", function() {
-      expect(thermostat.temperature).toEqual(20);
-    });
+    describe("increasing the temperature", function (){
+      it ("can increase the temperature if less than 32 degrees", function() {
+        thermostat.temperature = 25
+        thermostat.increaseTemperature()
+        expect(thermostat.temperature).toEqual(26)
+      })
 
-    it ("can increase the temperature", function() {
-      thermostat.increaseTemperature()
-      expect(thermostat.temperature).toEqual(21)
+      it("won't increase the temperature if over 32 degrees", function(){
+        thermostat.temperature = 32
+        thermostat.increaseTemperature()
+        expect(thermostat.temperature).toEqual(32)
+      })
     })
 
-    it ("can decrease the temperature", function () {
-      thermostat.decreaseTemperature()
-      expect(thermostat.temperature).toEqual(19)
-    });
- });
+    describe("decrease the temperature", function(){
+      it ("can decrease the temperature if current temperature is above 10 degrees", function () {
+        thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual(19)
+      });
+
+      it("should not be able to decrease below minimum temperature of 10 degrees", function() {
+        thermostat.temperature = 10
+        thermostat.decreaseTemperature()
+        expect(thermostat.temperature).toEqual(10)
+      });
+    })
+  })
+});
   
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,12 +1,18 @@
 function Thermostat() {
 	this.temperature = 20;
+	this.powerSaveMode = true;
 
 }
 
 Thermostat.prototype.increaseTemperature = function() {
-	this.temperature += 1;
+	if (this.temperature < this.maximumTemperature()) this.temperature += 1;
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
-	this.temperature -= 1;
+	if (this.temperature > 10) this.temperature -= 1;
 };
+
+Thermostat.prototype.maximumTemperature = function() {
+	return (this.powerSaveMode) ? 25 : 32;
+}
+
